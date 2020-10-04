cbfcb34ff29a1973a4e8db0dde3d57a92d00382b
Robot-Inventor <30.keita@gmail.com>
2020-10-04 16:57:44 +0900

    サンプルページのプログラムをv2.0.4.0-Budgerigarに更新

--- orizin_ui.js
+++ orizin_ui.js
@@ -1,175 +1,312 @@
-class ORIZIN_UI {
-    toggle_switch({
-        toggle_switch_target = "input[type=checkbox].toggle",
-        toggle_width = "2.5rem",
-        toggle_height = "1rem",
-        toggle_border_thickness = "0.05rem",
-        toggle_border_color = "gray",
-        toggle_disabled_color = "white",
-        toggle_enabled_color = "#adff99",
-        toggle_handle_color = "white",
-        toggle_handle_diameter = "calc(var(--toggle_height) + 0.4rem)"
-    } = {}) {
-        let toggle_element_num = 0;
-        document.querySelectorAll(toggle_switch_target).forEach(function(toggle_element) {
-            let toggle_id = toggle_element.getAttribute("id");
-            if (!toggle_id) {
-                toggle_element.setAttribute("id", "toggle_element_" + toggle_element_num);
-                toggle_id = "toggle_element_" + toggle_element_num;
-                toggle_element_num += 1;
+class ToggleSwitch extends HTMLElement {
+    static get observedAttributes() {
+        return ["checked"];
+    }
+
+    constructor() {
+        super();
+
+        this._checked = null;
+
+        const toggle_width = "2.5rem";
+        const toggle_height = "1rem";
+        const toggle_border_thickness = "0.05rem";
+        const toggle_border_color = "gray";
+        const toggle_disabled_color = "white";
+        const toggle_enabled_color = "#adff99";
+        const toggle_handle_color = "white";
+        const toggle_handle_diameter = `calc(${toggle_height} + 0.4rem)`;
+
+        const shadow = this.attachShadow({mode: "closed"});
+
+        const checkbox_element = document.createElement("input");
+        this._checkbox_element = checkbox_element;
+        checkbox_element.setAttribute("type", "checkbox");
+        checkbox_element.setAttribute("id", "checkbox");
+        if(this.hasAttribute("checked")) {
+            checkbox_element.checked = true;
+        }
+
+        const toggle_background_element = document.createElement("label");
+        toggle_background_element.setAttribute("for", "checkbox");
+        toggle_background_element.setAttribute("part", "foundation");
+
+        const toggle_handle_element = document.createElement("div");
+        toggle_handle_element.setAttribute("part", "handle");
+
+        const style = document.createElement("style");
+        style.textContent = `
+        #checkbox {
+            display: none;
+            transition: 0.3s;
+        }
+
+        #checkbox+label {
+            display: inline-block;
+            background: ${toggle_disabled_color};
+            width: ${toggle_width};
+            height: ${toggle_height};
+            border: solid ${toggle_border_thickness} ${toggle_border_color};
+            border-radius: ${toggle_width};
+            margin: calc(${toggle_handle_diameter} - ${toggle_height});
+            padding: 0;
+            vertical-align: bottom;
+            overflow: visible;
+            transition: 0.3s;
+            cursor: pointer;
+        }
+
+        #checkbox:checked+label {
+            background: ${toggle_enabled_color};
+        }
+
+        #checkbox+label div {
+            display: block;
+            background: ${toggle_handle_color};
+            width: ${toggle_handle_diameter};
+            height: ${toggle_handle_diameter};
+            border: ${toggle_border_color} solid ${toggle_border_thickness};
+            border-radius: ${toggle_width};
+            transition: 0.3s;
+            margin: 0;
+            position: relative;
+            top: calc(${toggle_border_thickness} * -1 - 0.2rem);
+            left: calc(${toggle_border_thickness} * -1 - 0.2rem);
+        }
+
+        #checkbox:checked+label div {
+            margin-left: 100%;
+            transform: translateX(-50%);
+        }
+
+        :host {
+            display: inline-block;
+        }
+        `;
+
+        shadow.appendChild(checkbox_element);
+        shadow.appendChild(toggle_background_element);
+        shadow.appendChild(style);
+        toggle_background_element.appendChild(toggle_handle_element);
+
+        checkbox_element.addEventListener("change", () => {
+            if(checkbox_element.checked) {
+                this.setAttribute("checked", "");
+            } else {
+                this.removeAttribute("checked");
             }
-            toggle_element.insertAdjacentHTML("afterend", "<label for='" + toggle_id + "'><div></div></label>");
+            const change_event = new CustomEvent("change");
+            this.dispatchEvent(change_event);
         });
 
-        this._insert_css(`
-            <style>
-                :root {
-                    --toggle_width: ${toggle_width};
-                    --toggle_height: ${toggle_height};
-                    --toggle_border_thickness: ${toggle_border_thickness};
-                    --toggle_border_color: ${toggle_border_color};
-                    --toggle_disabled_color: ${toggle_disabled_color};
-                    --toggle_enabled_color: ${toggle_enabled_color};
-                    --toggle_handle_color: ${toggle_handle_color};
-                    --toggle_handle_diameter: ${toggle_handle_diameter};
-                }
-
-                ${toggle_switch_target} {
-                    display: none;
-                    transition: 0.3s;
-                }
-
-                ${toggle_switch_target}+label {
-                    display: inline-block;
-                    background: var(--toggle_disabled_color);
-                    width: var(--toggle_width);
-                    height: var(--toggle_height);
-                    border: solid var(--toggle_border_thickness) var(--toggle_border_color);
-                    border-radius: var(--toggle_width);
-                    margin: calc(var(--toggle_handle_diameter) - var(--toggle_height));
-                    padding: 0;
-                    vertical-align: bottom;
-                    overflow: visible;
-                    transition: 0.3s;
-                    cursor: pointer;
-                }
-
-                ${toggle_switch_target}:checked+label {
-                    background: var(--toggle_enabled_color);
-                }
-
-                ${toggle_switch_target}+label div {
-                    display: block;
-                    content: "";
-                    background: var(--toggle_handle_color);
-                    width: var(--toggle_handle_diameter);
-                    height: var(--toggle_handle_diameter);
-                    border: var(--toggle_border_color) solid var(--toggle_border_thickness);
-                    border-radius: var(--toggle_width);
-                    box-shadow: 0 2px 5px var(--shadow);
-                    transition: 0.3s;
-                    margin: 0;
-                    position: relative;
-                    top: calc(var(--toggle_border_thickness) * -1 - 0.2rem);
-                    left: calc(var(--toggle_border_thickness) * -1 - 0.2rem);
-                }
-
-                ${toggle_switch_target}:checked+label div {
-                    margin-left: calc(100% - var(--toggle_height));
-                }
-            </style>
-        `);
-    }
-
-    // underline_textbox(underline_textbox_target="input[type=text].underline_textbox") {
-    underline_textbox({
-        underline_textbox_target = "input[type=text].underline_textbox",
-        underline_color_normal = "rgba(0, 0, 0, 0.7)",
-        underline_color_focused = "#ff6a00",
-        underline_thickness = "0.15rem",
-        underline_textbox_width = "50%"
-    } = {}) {
-        let input_element_num = 0;
-        document.querySelectorAll(underline_textbox_target).forEach(function(input_element) {
-            const input_group_id = "input_group_" + input_element_num;
-            input_element.insertAdjacentHTML("afterend", "<div class='text_input_group' id='" + input_group_id + "'><div class='text_underline_group'><div class='text_underline_normal'></div><div class='text_underline_focused'></div></div></div>");
-            const text_input_group = document.querySelector("div.text_input_group#" + input_group_id);
-            text_input_group.insertBefore(input_element, document.querySelector("div.text_input_group#" + input_group_id + " div.text_underline_group"));
-            input_element_num += 1;
+        checkbox_element.addEventListener("input", () => {
+            if(checkbox_element.checked) {
+                this.setAttribute("checked", "");
+            } else {
+                this.removeAttribute("checked");
+            }
         });
-
-        this._insert_css(`
-            <style>
-                :root {
-                    --underline_color_normal: ${underline_color_normal};
-                    --underline_color_focused: ${underline_color_focused};
-                    --underline_thickness: ${underline_thickness};
-                    --underline_textbox_width: ${underline_textbox_width};
-                }
-
-                .text_input_group {
-                    width: var(--underline_textbox_width);
-                    display: inline-block;
-                    margin: 0;
-                    padding: 0;
-                }
-
-                .text_input_group input {
-                    width: 100%;
-                    height: 100%;
-                    border: none;
-                    outline: none;
-                    background: transparent;
-                    margin: 0 0 var(--underline_thickness) 0;
-                    padding: var(--underline_thickness);
-                    box-sizing: border-box;
-                }
-
-                .text_input_group .text_underline_group {
-                    width: 100%;
-                    height: var(--underline_thickness);
-                    position: relative;
-                    left: 0;
-                    top: calc(-1.5 * var(--underline_thickness));
-                    margin: 0;
-                    padding: 0;
-                }
-
-                .text_input_group input + .text_underline_group .text_underline_normal {
-                    width: 100%;
-                    height: 100%;
-                    position: absolute;
-                    background: var(--underline_color_normal);
-                    border-radius: var(--underline_thickness);
-                    margin: 0;
-                    padding: 0;
-                }
-
-                .text_input_group input + .text_underline_group .text_underline_focused {
-                    width: 0;
-                    height: 100%;
-                    position: absolute;
-                    background: var(--underline_color_focused);
-                    transition: 0.3s;
-                    margin: 0 0 0 50%;
-                    padding: 0;
-                    border-radius: var(--underline_thickness);
-                }
-
-                .text_input_group input:focus + .text_underline_group .text_underline_focused {
-                    width: 100%;
-                    margin: 0;
-                }
-            </style>
-        `);
     }
 
-    all({toggle_switch_arg, underline_textbox_arg} = {}) {
-        this.toggle_switch(toggle_switch_arg);
-        this.underline_textbox(underline_textbox_arg);
+    attributeChangedCallback(name, old_value, new_value) {
+        switch(name) {
+            case "checked":
+                this._checked = new_value;
+                if(new_value || new_value === "") {
+                    this._checkbox_element.setAttribute("checked", "");
+                } else {
+                    this._checkbox_element.removeAttribute("checked");
+                }
+                break;
+        }
     }
 
-    _insert_css(content) {
-        document.body.insertAdjacentHTML("afterbegin", content);
+    get checked() {
+        return this.hasAttribute("checked");
+    }
+
+    set checked(value) {
+        if(value) {
+            this._checkbox_element.setAttribute("checked", "");
+            this.setAttribute("checked", "");
+        } else {
+            this._checkbox_element.removeAttribute("checked");
+            this.removeAttribute("checked");
+        }
     }
 }
+
+customElements.define("toggle-switch", ToggleSwitch);
+
+
+class UnderlineTextbox extends HTMLElement {
+    static get observedAttributes() {
+        return ["value", "autocomplete", "list", "maxlength", "minlength", "pattern", "placeholder", "readonly", "required", "size"];
+    }
+
+    constructor() {
+        super();
+
+        this._value = null;
+        this._autocomplete = null;
+        this._list = null;
+        this._maxlength = null;
+        this._minlength = null;
+        this._pattern = null;
+        this._placeholder = null;
+        this._readonly = null;
+        this._required = null;
+        this._size = null;
+
+        const underline_color_normal = "rgba(0, 0, 0, 0.7)";
+        const underline_color_focused = "#ff6a00";
+        const underline_thickness = "0.15rem";
+
+        const shadow = this.attachShadow({mode: "closed"});
+
+        const input_element = document.createElement("input");
+        this._input_element = input_element;
+        input_element.setAttribute("type", "text");
+        input_element.setAttribute("id", "textbox");
+        input_element.setAttribute("part", "textbox");
+
+        if(this.hasAttribute("value")) {
+            input_element.value = this.getAttribute("value");
+        }
+        if(this.hasAttribute("autocomplete")) {
+            input_element.setAttribute("autocomplete", this.getAttribute("autocomplete"));
+        }
+        if(this.hasAttribute("list")) {
+            input_element.setAttribute("list", this.getAttribute("list"));
+        }
+        if(this.hasAttribute("maxlength")) {
+            input_element.setAttribute("maxlength", this.getAttribute("maxlength"));
+        }
+        if(this.hasAttribute("minlength")) {
+            input_element.setAttribute("minlength", this.getAttribute("minlength"));
+        }
+        if(this.hasAttribute("pattern")) {
+            input_element.setAttribute("pattern", this.getAttribute("pattern"));
+        }
+        if(this.hasAttribute("placeholder")) {
+            input_element.setAttribute("placeholder", this.getAttribute("placeholder"));
+        }
+        if(this.hasAttribute("readonly")) {
+            input_element.setAttribute("readonly", this.getAttribute("readonly"));
+        }
+        if(this.hasAttribute("required")) {
+            input_element.setAttribute("required", this.getAttribute("required"));
+        }
+        if(this.hasAttribute("size")) {
+            input_element.setAttribute("size", this.getAttribute("size"));
+        }
+
+        const underline_group = document.createElement("div");
+        underline_group.setAttribute("id", "underline_group");
+
+        const normal_underline = document.createElement("div");
+        normal_underline.setAttribute("id", "normal_underline");
+        normal_underline.setAttribute("part", "normal_underline");
+
+        const focused_underline = document.createElement("div");
+        focused_underline.setAttribute("id", "focused_underline");
+        focused_underline.setAttribute("part", "focused_underline");
+
+        const style = document.createElement("style");
+        style.textContent = `
+        #textbox {
+            width: 100%;
+            height: 100%;
+            border: none;
+            outline: none;
+            background: transparent;
+            margin: 0 0 ${underline_thickness} 0;
+            padding: ${underline_thickness};
+            box-sizing: border-box;
+        }
+
+        #textbox + #underline_group {
+            width: 100%;
+            height: ${underline_thickness};
+            position: relative;
+            left: 0;
+            top: calc(-1.5 * ${underline_thickness});
+            margin: 0;
+            padding: 0;
+        }
+
+        #textbox + #underline_group #normal_underline {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            background: ${underline_color_normal};
+            border-radius: ${underline_thickness};
+            margin: 0;
+            padding: 0;
+        }
+
+        #textbox + #underline_group #focused_underline {
+            width: 0;
+            height: 100%;
+            position: absolute;
+            background: ${underline_color_focused};
+            transition: 0.3s;
+            margin: 0 0 0 50%;
+            padding: 0;
+            border-radius: ${underline_thickness};
+        }
+
+        #textbox:focus + #underline_group #focused_underline {
+            width: 100%;
+            margin: 0;
+        }
+
+        :host {
+            display: inline-block;
+        }
+        `;
+
+        shadow.appendChild(input_element);
+        underline_group.appendChild(normal_underline);
+        underline_group.appendChild(focused_underline);
+        shadow.appendChild(underline_group);
+        shadow.appendChild(style);
+
+        input_element.addEventListener("change", () => {
+            this.setAttribute("value", input_element.value);
+            const change_event = new CustomEvent("change");
+            this.dispatchEvent(change_event);
+        });
+
+        input_element.addEventListener("input", () => {
+            this.setAttribute("value", input_element.value);
+        });
+
+        Object.keys(this).forEach(function(property_name) {
+            const attribute_name = property_name.replace(/^_+/, '');
+            Object.defineProperty(this, attribute_name, {
+                get: function() {
+                    return this.getAttribute(attribute_name);
+                },
+                set: function(v) {
+                    this.setAttribute(attribute_name, v);
+                    this._input_element.setAttribute(attribute_name, v);
+                }
+            });
+        }, this);
+    }
+
+    attributeChangedCallback(name, old_value, new_value) {
+        function change_attribute(attribute_name) {
+            const property_name = "_" + attribute_name;
+            this[property_name] = new_value;
+            this._input_element.setAttribute(attribute_name, new_value);
+        }
+
+        if(name in ["value", "autocomplete", "list", "maxlength", "minlength", "pattern", "placeholder", "readonly", "required", "size"]) {
+            change_attribute.bind(this)(name);
+        }
+    }
+}
+
+customElements.define("underlined-textbox", UnderlineTextbox);
